96f8c6724669875afc95987aad52bade5e91eb6c
bcamus <benjamin.camus@inria.fr>
2019-01-02 17:12:45 +0100

    start implementing socket for VMinterface

--- src/include/VmsInterface.hpp
+++ src/include/VmsInterface.hpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 namespace vsg{
 
@@ -16,32 +18,15 @@ struct message{
 class VmsInterface{
 
   public:
-    virtual bool vmActive() = 0;
-    virtual std::vector<message> goTo(double deadline) = 0; 
-    virtual std::string getHostOfVm(std::string vm_name) = 0;
-    virtual void deliverMessage(message m) = 0;
-};
-
-class DummyVmsInterface: public VmsInterface{
-  
-  private:
-    const int message_size = 100;
-
-    std::unordered_map<std::string, std::string> dest_of_vm;
-    int nb_packet_send;
-
-    std::vector<std::string> vm_names;
-    std::unordered_map<std::string, std::string> host_of_vms;
-    std::unordered_map<std::string, std::vector<double>> vms_sending_times;
-  
-  public:
-    DummyVmsInterface();
-    ~DummyVmsInterface();
-
+    VmsInterface(std::vector<std::string> vm_names);
+    ~VmsInterface();
     bool vmActive();
-    std::vector<message> goTo(double deadline);
+    std::vector<message> goTo(double deadline); 
     std::string getHostOfVm(std::string vm_name);
-    void deliverMessage(message m); 
+    void deliverMessage(message m);
+
+  private:
+    std::unordered_map<std::string, int> vm_sockets;
 };
 
 }
