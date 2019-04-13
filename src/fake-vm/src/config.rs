b7271b503ffcac5376ae1fdff235b99fd6be7480
Louis Rilling <l.rilling@av7.net>
2019-04-14 01:38:10 +0200

    fake-vm: Make a cleaner separation between Rust-level API and FFI API

--- src/fake-vm/src/config.rs
+++ src/fake-vm/src/config.rs
@@ -1,5 +1,4 @@
 use chrono::naive::NaiveDateTime;
-use std::os::raw::{c_char, c_int};
 use std::str::FromStr;
 use structopt::StructOpt;
 
@@ -15,104 +14,60 @@ pub(crate) struct Config {
     pub time_offset: NaiveDateTime,
 }
 
-impl Config {
-    pub(super) unsafe fn from_os_args(argc: c_int, argv: *const *const c_char) -> Result<(Config, c_int), structopt::clap::Error> {
-        use std::os::unix::ffi::OsStrExt;
-
-        let mut next_arg: c_int = 0;
-        let args = (0..argc).filter_map(|i| {
-            let str_arg = std::ffi::OsStr::from_bytes(std::ffi::CStr::from_ptr(*argv.offset(i as isize)).to_bytes());
-            if next_arg == 0 && str_arg == "--" {
-                next_arg = (i + 1) as c_int;
-            }
-            if next_arg == 0 {
-                Some(std::borrow::Cow::from(str_arg))
-            } else {
-                None
-            }
-        });
-
-        let config = Config::from_iter_safe(args)?;
-        if next_arg == 0 {
-            next_arg = argc;
-        }
-        Ok((config, next_arg))
-    }
-}
-
 #[cfg(test)]
 mod test {
-    use crate::os_args;
+    use structopt::StructOpt;
     use super::*;
 
     #[test]
-    // Correct args and next args index in case of sticked options and values
+    // Correct args in case of sticked options and values
     fn valid_args1() {
-        let (config, next) = unsafe { Config::from_os_args(2, os_args!("-atiti", "-t1970-01-02T00:00:00")) }.unwrap();
-        assert_eq!(2, next);
+        let config = Config::from_iter_safe(&["-atiti", "-t1970-01-02T00:00:00"]);
+        assert!(config.is_ok());
 
+        let config = config.unwrap();
         assert_eq!("titi", config.actor_socket.to_str().unwrap());
         assert_eq!(NaiveDateTime::from_timestamp(86400, 0), config.time_offset);
     }
 
     #[test]
-    // Correct args and next args index in case of split options and values
+    // Correct args in case of split options and values
     fn valid_args2() {
-        let (config, next) = unsafe { Config::from_os_args(4, os_args!("-a", "titi", "-t", "1970-01-02T00:00:00")) }.unwrap();
-        assert_eq!(4, next);
+        let config = Config::from_iter_safe(&["-a", "titi", "-t", "1970-01-02T00:00:00"]);
+        assert!(config.is_ok());
 
+        let config = config.unwrap();
         assert_eq!("titi", config.actor_socket.to_str().unwrap());
         assert_eq!(NaiveDateTime::from_timestamp(86400, 0), config.time_offset);
-    }
-
-    #[test]
-    // Next args start right after "--"
-    fn valid_args3() {
-        let (_config, next) = unsafe { Config::from_os_args(3, os_args!("-atiti", "-t1970-01-02T00:00:00", "--")) }.unwrap();
-        assert_eq!(3, next);
-    }
-
-    #[test]
-    // Next args start right after "--"
-    fn valid_args4() {
-        let (_config, next) = unsafe { Config::from_os_args(4, os_args!("-atiti", "-t1970-01-02T00:00:00", "--", "other arg")) }.unwrap();
-        assert_eq!(3, next);
-    }
-
-    #[test]
-    // Next args start right after the first occurence of "--"
-    fn valid_args5() {
-        let (_config, next) = unsafe { Config::from_os_args(4, os_args!("-atiti", "-t1970-01-02T00:00:00", "--", "--")) }.unwrap();
-        assert_eq!(3, next);
     }
 
     #[test]
     // Missing socket value
     fn invalid_args1() {
-        assert!(unsafe { Config::from_os_args(2, os_args!("-a", "-t1970-01-02T00:00:00")) }.is_err());
+        assert!(Config::from_iter_safe(&["-a", "-t1970-01-02T00:00:00"]).is_err());
     }
 
     #[test]
     // Missing time value
     fn invalid_args2() {
-        assert!(unsafe { Config::from_os_args(3, os_args!("-a", "titi", "-t")) }.is_err());
+        assert!(Config::from_iter_safe(&["-a", "titi", "-t"]).is_err());
     }
 
     #[test]
     // Missing time
     fn invalid_args3() {
-        assert!(unsafe { Config::from_os_args(2, os_args!("-a", "titi")) }.is_err());
+        assert!(Config::from_iter_safe(&["-a", "titi"]).is_err());
     }
 
     #[test]
     // Missing actor_socket
     fn invalid_args4() {
-        assert!(unsafe { Config::from_os_args(1, os_args!("-t1970-01-02T00:00:00")) }.is_err());
+        assert!(Config::from_iter_safe(&["-t1970-01-02T00:00:00"]).is_err());
     }
 
     #[test]
     // Invalid time format
     fn invalid_args5() {
-        assert!(unsafe { Config::from_os_args(2, os_args!("-atiti", "-t1970-01-02T00:00")) }.is_err());
+        assert!(Config::from_iter_safe(&["-atiti", "-t1970-01-02T00:00"]).is_err());
     }
 }
--- src/fake-vm/src/lib.rs
+++ src/fake-vm/src/lib.rs
@@ -7,6 +7,7 @@ pub(crate) use config::Config;
 use connector::{Connector, ConnectorImpl, MsgIn, MsgOut};
 use libc;
 use log::{debug, error};
+use std::io::Result;
 use std::os::raw::{c_char, c_int};
 
 const MAX_PACKET_SIZE: usize = 2048;
@@ -21,7 +22,7 @@ pub struct Context {
 }
 
 impl Context {
-    fn new(config: &Config) -> std::io::Result<Box<Context>> {
+    fn new(config: &Config) -> Result<Box<Context>> {
         // Here is where the time reference is recorded
         let time_offset = config.time_offset - chrono::offset::Local::now().naive_local();
         let (connector, input_buffer) = ConnectorImpl::new(&config)?;
@@ -35,28 +36,60 @@ impl Context {
 }
 
 #[no_mangle]
-pub unsafe extern fn vsg_init(argc: c_int, argv: *const *const c_char, next_arg: *mut c_int) -> *mut Context {
-    let (config, next) = match Config::from_os_args(argc, argv) {
-        Ok(r) => r,
-        Err(e) => {
-            error!("vsg_init failed: {}", e);
-            return std::ptr::null_mut();
-        },
-    };
-    debug!("{:?}", config);
-
-    match Context::new(&config) {
-        Ok(context) => {
-            if let Some(next_arg) = next_arg.as_mut() {
-                *next_arg = next;
+pub unsafe extern fn vsg_init(argc: c_int, argv: *const *const c_char, next_arg_p: *mut c_int) -> *mut Context {
+    match parse_os_args(argc, argv, |args| init(args)) {
+        Ok((context, next_arg)) => {
+            if let Some(next_arg_p) = next_arg_p.as_mut() {
+                *next_arg_p = next_arg;
             }
             Box::into_raw(context)
         },
         Err(e) => {
             error!("vsg_init failed: {}", e);
             std::ptr::null_mut()
-        }
+        },
     }
+}
+
+unsafe fn parse_os_args<F, T>(argc: c_int, argv: *const *const c_char, parse: F) -> Result<(T, c_int)>
+    where F: FnOnce(&mut Iterator<Item = std::borrow::Cow<'static, std::ffi::OsStr>>) -> Result<T>
+{
+    use std::os::unix::ffi::OsStrExt;
+
+    let mut next_arg = None;
+    let mut args = (0..argc).filter_map(|i| {
+        let str_arg = std::ffi::OsStr::from_bytes(std::ffi::CStr::from_ptr(*argv.offset(i as isize)).to_bytes());
+        if next_arg.is_none() && str_arg == "--" {
+            next_arg = Some(i + 1);
+        }
+        if next_arg.is_none() {
+            Some(std::borrow::Cow::from(str_arg))
+        } else {
+            None
+        }
+    });
+
+    match parse(&mut args) {
+        Ok(r) => {
+            let next_arg = match next_arg {
+                Some(n) => n,
+                None => argc,
+            };
+            Ok((r, next_arg))
+        },
+        Err(e) => Err(e),
+    }
+}
+
+pub fn init<I>(args: I) -> Result<Box<Context>>
+    where I: IntoIterator,
+          I::Item: Into<std::ffi::OsString> + Clone {
+    use structopt::StructOpt;
+
+    let config = Config::from_iter_safe(args).or_else(|e| Err(std::io::Error::new(std::io::ErrorKind::Other, e)))?;
+    debug!("{:?}", config);
+
+    Context::new(&config)
 }
 
 #[no_mangle]
@@ -92,21 +125,52 @@ fn handle_actor_msg(connector: &mut ConnectorImpl, msg: MsgIn) {
 }
 
 #[cfg(test)]
-#[macro_export]
-macro_rules! os_args {
-    ( $( $s:expr ),* ) => {
-        [$(std::ffi::CString::new($s).unwrap().as_ptr(),)*].as_ptr()
-    }
-}
-
-#[cfg(test)]
 mod test {
     use log::{error, info};
+    use std::ffi::CString;
+    use std::os::raw::{c_char, c_int};
+    use std::os::unix::ffi::OsStrExt;
     use std::path::PathBuf;
-    use std::os::raw::c_int;
     use stderrlog::StdErrLog;
     use super::*;
     use super::connector::test_helpers::*;
+
+    struct OsArgs {
+        raw: Vec<CString>,
+        os: Vec<*const c_char>,
+    }
+
+    impl OsArgs {
+        fn raw(&self) -> &[CString] {
+            self.raw.as_slice()
+        }
+
+        fn argc(&self) -> c_int {
+            self.raw.len() as c_int
+        }
+
+        fn argv(&self) -> *const *const c_char {
+            self.os.as_ptr()
+        }
+    }
+
+    macro_rules! os_args {
+        ( $( $s:expr ),* ) => {{
+            let mut os_args = OsArgs {
+                raw: Vec::new(),
+                os: Vec:: new(),
+            };
+
+            $({
+                let arg = CString::new($s).unwrap();
+                let ptr = arg.as_ptr();
+                os_args.raw.push(arg);
+                os_args.os.push(ptr);
+            })*
+
+            os_args
+        }}
+    }
 
     macro_rules! valid_args {
         () => {
@@ -132,6 +196,82 @@ mod test {
         INIT.call_once(|| stderrlog::new().module(module_path!()).verbosity(log::Level::Info as usize).init().unwrap());
     }
 
+    fn parse_args_compare<I>(raw_args: &[CString], args: I, num_required_args: c_int) -> Result<()>
+        where I: IntoIterator,
+              I::Item: Into<std::ffi::OsString> + Clone
+    {
+        assert!(raw_args.len() >= num_required_args as usize);
+
+        let mut count = 0;
+        raw_args.iter().zip(args).all(|(r, a)| {
+            count += 1;
+            if r.as_bytes() == a.into().as_bytes() {
+                true
+            } else {
+                false
+            }
+        });
+
+        if num_required_args == count {
+            Ok(())
+        } else if num_required_args > count {
+            Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("raw_args and args differ at index {}", count - 1)))
+        } else {
+            Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("parse_os_args() iterated {} args too far", count - num_required_args)))
+        }
+    }
+
+    #[test]
+    // parse_os_args() correctly iterates over all args and returns the right next args index
+    fn parse_os_args1() {
+        let args = os_args!("-atiti", "-t1970-01-02T00:00:00");
+        let res = unsafe { parse_os_args(args.argc(), args.argv(), |a| parse_args_compare(args.raw(), a, 2)) };
+        assert!(res.is_ok());
+        let (_, next) = res.unwrap();
+        assert_eq!(2, next);
+    }
+
+    #[test]
+    // parse_os_args() correctly iterates over all args and returns the right next args index
+    // no special handling of split options
+    fn parse_os_args2() {
+        let args = os_args!("-a", "titi", "-t", "1970-01-02T00:00:00");
+        let res = unsafe { parse_os_args(args.argc(), args.argv(), |a| parse_args_compare(args.raw(), a, 4)) };
+        assert!(res.is_ok());
+        let (_, next) = res.unwrap();
+        assert_eq!(4, next);
+    }
+
+    #[test]
+    // Next args start right after "--"
+    fn parse_os_args3() {
+        let args = os_args!("-atiti", "-t1970-01-02T00:00:00", "--");
+        let res = unsafe { parse_os_args(args.argc(), args.argv(), |a| parse_args_compare(args.raw(), a, 2)) };
+        assert!(res.is_ok());
+        let (_, next) = res.unwrap();
+        assert_eq!(3, next);
+    }
+
+    #[test]
+    // Next args start right after "--"
+    fn parse_os_args4() {
+        let args = os_args!("-atiti", "-t1970-01-02T00:00:00", "--", "other arg");
+        let res = unsafe { parse_os_args(args.argc(), args.argv(), |a| parse_args_compare(args.raw(), a, 2)) };
+        assert!(res.is_ok());
+        let (_, next) = res.unwrap();
+        assert_eq!(3, next);
+    }
+
+    #[test]
+    // Next args start right after the first occurence of "--"
+    fn parse_os_args5() {
+        let args = os_args!("-atiti", "-t1970-01-02T00:00:00", "--", "--");
+        let res = unsafe { parse_os_args(args.argc(), args.argv(), |a| parse_args_compare(args.raw(), a, 2)) };
+        assert!(res.is_ok());
+        let (_, next) = res.unwrap();
+        assert_eq!(3, next);
+    }
+
     #[test]
     fn init_valid() {
         init();
@@ -139,7 +279,8 @@ mod test {
         let mut next_arg: c_int = 0;
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, valid_args!(), &mut next_arg) };
+        let args = valid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), &mut next_arg) };
         assert!(!context.is_null());
         assert_eq!(2, next_arg);
 
@@ -156,7 +297,8 @@ mod test {
 
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, valid_args!(), std::ptr::null_mut()) };
+        let args = valid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), std::ptr::null_mut()) };
         assert!(!context.is_null());
 
         let context = unsafe { context.as_mut() }.unwrap();
@@ -173,7 +315,8 @@ mod test {
         let mut next_arg: c_int = 0;
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, invalid_args!(), &mut next_arg) };
+        let args = invalid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), &mut next_arg) };
         assert!(context.is_null());
         assert_eq!(0, next_arg);
 
@@ -186,7 +329,8 @@ mod test {
 
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, invalid_args!(), std::ptr::null_mut()) };
+        let args = invalid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), std::ptr::null_mut()) };
         assert!(context.is_null());
 
         test_cleanup_connect(&server_path);
@@ -205,7 +349,8 @@ mod test {
 
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, valid_args!(), std::ptr::null_mut()) };
+        let args = valid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), std::ptr::null_mut()) };
         assert!(!context.is_null());
 
         let mut tv: libc::timeval = libc::timeval {tv_sec: 0, tv_usec: 0};
@@ -225,7 +370,8 @@ mod test {
 
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, valid_args_h1!(), std::ptr::null_mut()) };
+        let args = valid_args_h1!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), std::ptr::null_mut()) };
         assert!(!context.is_null());
 
         let mut tv: libc::timeval = libc::timeval {tv_sec: 0, tv_usec: 0};
@@ -245,7 +391,8 @@ mod test {
 
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, valid_args!(), std::ptr::null_mut()) };
+        let args = valid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), std::ptr::null_mut()) };
         assert!(!context.is_null());
 
         let res: c_int = unsafe { vsg_gettimeofday(context, std::ptr::null_mut(), std::ptr::null_mut()) };
@@ -261,7 +408,8 @@ mod test {
 
         let server_path = PathBuf::from("titi");
         test_prepare_connect(&server_path, test_dummy_actor);
-        let context = unsafe { vsg_init(2, valid_args!(), std::ptr::null_mut()) };
+        let args = valid_args!();
+        let context = unsafe { vsg_init(args.argc(), args.argv(), std::ptr::null_mut()) };
         assert!(!context.is_null());
 
         if let Some(context) = unsafe { context.as_mut() } {
