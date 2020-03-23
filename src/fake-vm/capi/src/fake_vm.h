398c9720276568ec0bb63ad3613a97d8b80faaa3
Louis Rilling <louis.rilling@irisa.fr>
2020-03-23 18:45:50 +0100

    fake-vm/capi: Add fake_vm.h for client C applications

---
+++ src/fake-vm/capi/src/fake_vm.h
@@ -0,0 +1,20 @@
+#ifndef __FAKE_VM_H__
+#define __FAKE_VM_H__
+
+#include <stdint.h>
+#include <sys/time.h>
+
+struct vsg_context;
+
+typedef void (*vsg_recv_cb )(const struct vsg_context *context, uint32_t msglen, const uint8_t *msg);
+
+struct vsg_context *vsg_init(int argc, const char *const argv[], int *next_arg_p, vsg_recv_cb recv_cb);
+void vsg_cleanup(struct vsg_context *context);
+
+int vsg_start(const struct vsg_context *context);
+int vsg_stop(const struct vsg_context *context);
+
+int vsg_gettimeofday(const struct vsg_context *context, struct timeval *timeval, void *timezone);
+int vsg_send(const struct vsg_context *context, uint32_t msglen, const uint8_t *msg);
+
+#endif /* __FAKE_VM_H__ */
