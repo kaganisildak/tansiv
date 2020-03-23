53f82cc9fa50e110ec5467acf38f485860c8edff
Louis Rilling <louis.rilling@irisa.fr>
2020-03-23 23:58:51 +0100

    fake-vm: Make $(make test) run tests not managed by cargo

---
+++ src/fake-vm/capi/tests/build_test.c
@@ -0,0 +1,48 @@
+#include <fake_vm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void recv_cb(const struct vsg_context *context, uint32_t msglen, const uint8_t *msg)
+{
+}
+
+void die(const char *msg, int error)
+{
+	fprintf(stderr, "%s", msg);
+	if (error)
+		fprintf(stderr, "\t%s\n", strerror(error));
+	exit(1);
+}
+
+int main(int argc, const char *argv[])
+{
+	struct vsg_context *context;
+	struct timeval time;
+	unsigned char msg[] = "Foo msg";
+	int res;
+
+	context = vsg_init(argc, argv, NULL, recv_cb);
+	if (!context)
+		die("vsg_init() failed", 0);
+
+	res = vsg_start(context);
+	if (res)
+		die("vsg_start() failed", res);
+
+	res = vsg_gettimeofday(context, &time, NULL);
+	if (res)
+		die("vsg_gettimeofday() failed", res);
+
+	res = vsg_send(context, sizeof(msg), msg);
+	if (res)
+		die("vsg_send() failed", res);
+
+	res = vsg_stop(context);
+	if (res)
+		die("vsg_stop() failed", res);
+
+	vsg_cleanup(context);
+
+	return 0;
+}
