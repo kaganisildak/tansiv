a713b8fecdd0565784a432e0c109f84c2b4ba112
msimonin <matthieu.simonin@inria.fr>
2020-11-23 09:48:49 +0100

    packer: fix nas benchmarks dir permissions

--- packer/scripts/debian.sh
+++ packer/scripts/debian.sh
@@ -6,7 +6,19 @@ apt-get -y install software-properties-common
 apt-add-repository non-free
 apt-get -y update
 
-apt-get -y install cloud-init iperf
+apt-get -y install cloud-init iperf mpi-default-bin wget vim
+
+# Get the NAS parallel benchmark
+mkdir -p /opt
+cd /opt
+wget https://www.nas.nasa.gov/assets/npb/NPB3.3.1.tar.gz
+tar -xvzf NPB3.3.1.tar.gz
+chown -R tansiv:tansiv .
+
+mkdir -p /home/tansiv/.ssh
+ssh-keygen -t rsa -f /home/tansiv/.ssh/id_rsa -P ''
+cat /home/tansiv/.ssh/id_rsa.pub >> /home/tansiv/.ssh/authorized_keys
+chown tansiv:tansiv -R /home/tansiv/.ssh
 
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
 apt-get -y purge ppp pppconfig pppoeconf
