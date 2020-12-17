f37c165794cf1206095148ef8aac2e744a9f9872
msimonin <matthieu.simonin@inria.fr>
2020-12-17 09:43:08 +0100

    packer: add flent/netperf

--- packer/scripts/debian.sh
+++ packer/scripts/debian.sh
@@ -3,12 +3,24 @@
 apt -y update
 
 # some utils
-apt -y install cloud-init iperf wget vim
+# apt-get -y install software-properties-common
+# apt-add-repository non-free
+apt-get -y update
+
+apt -y install cloud-init wget vim git autotools-dev automake texinfo
+
+# ok we'll need netperf which isn't in buster/non-free !
+# let's get it the old way
+git clone https://github.com/HewlettPackard/netperf.git
+cd netperf
+./autogen.sh
+./configure --enable-demo --prefix /usr/local
+make && make install
 
 # some apps to validate the bouzin
 # since we have ssh ready on the management interface, we could also install
 # some apps on the fly
-apt -y install taktuk fping
+apt -y install taktuk fping iperf flent python3-matplotlib python3-setuptools
 
 mkdir -p /home/tansiv/.ssh
 ssh-keygen -t rsa -f /home/tansiv/.ssh/id_rsa -P ''
