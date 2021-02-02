59e938dccbee8925c9c9e5a914918b95a8eed4c1
Martin Quinson <martin.quinson@ens-rennes.fr>
2021-02-02 16:27:15 +0100

    Use hard links, not soft ones, to files that live out of the repo

--- internship/2020-mystere.c
+++ internship/2020-mystere.c
@@ -1,1 +1,47 @@
-/home/mquinson/Teaching/rennes/itw/itw-sysprog/2020-mystere.c
+#include <stdio.h>
+#include <ctype.h> // islower()
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){  
+  int A[2], B[2];
+  char c;
+
+  pipe(A); 
+  pipe(B);
+
+  if (fork()) {
+    if (fork()) {
+      close(A[0]); 
+      close(B[0]);
+      while (read(0,&c,1) == 1) {
+        if (islower(c)) {
+          write(A[1], &c, 1);
+        } else {
+          write(B[1], &c, 1);
+        }
+      }
+      close(A[1]); 
+      close(B[1]);
+      wait(NULL);
+      wait(NULL);
+    } else {
+      dup2(B[0],0);
+      close(A[0]); 
+      close(A[1]); 
+      close(B[0]);
+      close(B[1]);
+      while (read(0,&c,1) == 1) 
+        printf("1: %c\n", c);
+    }
+  } else {
+      dup2(A[0],0);
+      close(A[0]);
+      close(A[1]);
+      close(B[0]); 
+      close(B[1]); 
+      while (read(0,&c,1) == 1) 
+        printf("2: %c\n", c);
+  }
+  return 0;
+}
--- internship/2020-planete.c
+++ internship/2020-planete.c
@@ -1,1 +1,28 @@
-/home/mquinson/Teaching/rennes/itw/itw-sysprog/2020-planete.c
+#include <stdio.h>
+
+typedef struct {
+   char* name;
+   int radius;
+} planet_t;
+
+planet_t solar_sys[8];
+
+planet_t* planet_new(const char* name, int radius);
+void planet_free(planet_t* p);
+planet_t** read_system(const char*nom_fichier);
+
+int main(int argc, char** argv) {
+   planet_t jupiter = { "jupiter", 69911};
+
+   planet_t **planets = read_system("system.txt");
+
+   printf("%s %d\n", jupiter.name, jupiter.radius);
+   for (planet_t* curr=planets; cour != NULL; ?????) {
+      printf("%s %d\n", ????, ????); // Displays the info of the current planet
+   }
+
+   // Free the memory that was allocated by read_system()
+
+   return 0;
+}
+
