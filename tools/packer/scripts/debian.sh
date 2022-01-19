76fdca497c7866fa2f13ee64d3cef2a3e82bdcd9
msimonin <matthieu.simonin@inria.fr>
2022-01-19 10:32:06 +0100

    packer: netperf is back in bullseye/non-free

--- tools/packer/scripts/debian.sh
+++ tools/packer/scripts/debian.sh
@@ -3,24 +3,23 @@
 apt -y update
 
 # some utils
-# apt-get -y install software-properties-common
-# apt-add-repository non-free
+apt-get -y install software-properties-common
+apt-add-repository non-free
 apt-get -y update
 
 apt -y install cloud-init wget vim git autotools-dev automake texinfo tmux
 
-# ok we'll need netperf which isn't in buster/non-free !
-# let's get it the old way
-git clone https://github.com/HewlettPackard/netperf.git
-cd netperf
-./autogen.sh
-./configure --enable-demo --prefix /usr/local
-make && make install
-
 # some apps to validate the bouzin
 # since we have ssh ready on the management interface, we could also install
 # some apps on the fly
-apt -y install taktuk fping iperf flent python3-matplotlib python3-setuptools redis-server
+apt -y install taktuk \
+    fping \
+    netperf \
+    iperf \
+    flent \
+    python3-matplotlib \
+    python3-setuptools \
+    redis-server
 
 mkdir -p /home/tansiv/.ssh
 ssh-keygen -t rsa -f /home/tansiv/.ssh/id_rsa -P ''
