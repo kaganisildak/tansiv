cffc2bfd84782f1a55cca39d3c22878a0b5bda80
Léo Cosseron <leo.cosseron@irisa.fr>
2024-06-27 10:18:57 +0200

    Coordinator: Use clang-format

--- src/coordinator/VmsInterface.cpp
+++ src/coordinator/VmsInterface.cpp
@@ -8,32 +8,27 @@
 
 #include "socket.hpp"
 
-
 #define LOG(msg) std::clog << "[Interface:] " << msg << std::endl
 
 // big enough buffer for incoming messages
 #define SCRATCH_BUFFER_LEN 2048
 
-#define LOG_MESSAGES 1
+// #define LOG_MESSAGES 1
 
 // Enable to get some log about message create/copy/move
 // #define LOG_MESSAGES 1
 
 namespace vsg {
 
-bool sortMessages(Message* i, Message* j)
-{
-  return i->sent_time < j->sent_time;
-}
+bool sortMessages(Message *i, Message *j) { return i->sent_time < j->sent_time; }
 
-vsg_time simgridToVmTime(double simgrid_time)
-{
+vsg_time simgridToVmTime(double simgrid_time) {
   struct vsg_time vm_time;
 
   // when only one VM remains, simgrid_time = DOUBLE_MAX.
   // we use the code below to avoid conversion issue of DOUBLE_MAX to uint64_t.
   if (simgrid_time > std::numeric_limits<uint64_t>::max()) {
-    vm_time.seconds  = std::numeric_limits<uint64_t>::max();
+    vm_time.seconds = std::numeric_limits<uint64_t>::max();
     vm_time.nseconds = std::numeric_limits<uint64_t>::max();
   } else {
     // the simgrid time correspond to a double in second, so the number of seconds is the integer part
@@ -45,22 +40,15 @@ vsg_time simgridToVmTime(double simgrid_time)
   return vm_time;
 }
 
-double vmToSimgridTime(uint64_t seconds, uint64_t nseconds)
-{
-  return seconds + nseconds * 1e-9;
-}
+double vmToSimgridTime(uint64_t seconds, uint64_t nseconds) { return seconds + nseconds * 1e-9; }
 
-double vmToSimgridTime(vsg_time vm_time)
-{
-  return vmToSimgridTime(vm_time.seconds, vm_time.nseconds);
-}
+double vmToSimgridTime(vsg_time vm_time) { return vmToSimgridTime(vm_time.seconds, vm_time.nseconds); }
 
-VmsInterface::VmsInterface(std::string connection_socket_name, bool stop_at_any_stop)
-{
-  a_vm_stopped              = false;
-  simulate_until_any_stop   = stop_at_any_stop;
-  socket_name               = connection_socket_name;
-  const char* c_socket_name = socket_name.c_str();
+VmsInterface::VmsInterface(std::string connection_socket_name, bool stop_at_any_stop) {
+  a_vm_stopped = false;
+  simulate_until_any_stop = stop_at_any_stop;
+  socket_name = connection_socket_name;
+  const char *c_socket_name = socket_name.c_str();
 
   remove(c_socket_name);
   connection_socket = socket(PF_LOCAL, SOCK_STREAM, 0);
@@ -70,7 +58,7 @@ VmsInterface::VmsInterface(std::string connection_socket_name, bool stop_at_any_
   address.sun_family = AF_LOCAL;
   strcpy(address.sun_path, c_socket_name);
 
-  if (bind(connection_socket, (sockaddr*)(&address), sizeof(address)) != 0) {
+  if (bind(connection_socket, (sockaddr *)(&address), sizeof(address)) != 0) {
     std::perror("unable to bind connection socket");
     end_simulation();
   }
@@ -85,53 +73,49 @@ VmsInterface::VmsInterface(std::string connection_socket_name, bool stop_at_any_
   signal(SIGPIPE, SIG_IGN);
 }
 
-VmsInterface::~VmsInterface()
-{
-  end_simulation(true, false);
-}
+VmsInterface::~VmsInterface() { end_simulation(true, false); }
 
 void VmsInterface::register_vm(std::string host_name, std::string vm_name, std::string file,
-                               std::vector<std::string> argv)
-{
+                               std::vector<std::string> argv) {
   vm_deployments[vm_name] = host_name;
 
-  std::vector<char*> command;
+  std::vector<char *> command;
   // we inject the socket name as the first parameter
   // we thus have a convention here for the program launched by tansiv
   // they must handle the first parameter as the socket name.
   std::string exec_line = file;
-  for (auto const& arg : argv) {
-    command.emplace_back(const_cast<char*>(arg.c_str()));
+  for (auto const &arg : argv) {
+    command.emplace_back(const_cast<char *>(arg.c_str()));
     exec_line += " " + arg;
   }
   command.push_back(nullptr);
   auto it = command.begin();
-  command.insert(it + 1, (char*)socket_name.c_str());
+  command.insert(it + 1, (char *)socket_name.c_str());
 
   LOG("fork and exec of " << exec_line);
 
   switch (fork()) {
-    case -1:
-      std::perror("unable to fork process");
-      end_simulation();
-      break;
-    case 0:
-      end_simulation(false, false);
+  case -1:
+    std::perror("unable to fork process");
+    end_simulation();
+    break;
+  case 0:
+    end_simulation(false, false);
 
-      // inject the socket name as the first parameter
-      if (execvp(file.c_str(), command.data()) != 0) {
-        std::perror("unable to launch VM");
-        end_simulation();
-      }
-      break;
-    default:
-      break;
+    // inject the socket name as the first parameter
+    if (execvp(file.c_str(), command.data()) != 0) {
+      std::perror("unable to launch VM");
+      end_simulation();
+    }
+    break;
+  default:
+    break;
   }
   LOG("fork done for VM " << vm_name);
 
   struct sockaddr_un vm_address = {0};
-  unsigned int len              = sizeof(vm_address);
-  int vm_socket                 = accept(connection_socket, (sockaddr*)(&vm_address), &len);
+  unsigned int len = sizeof(vm_address);
+  int vm_socket = accept(connection_socket, (sockaddr *)(&vm_address), &len);
   if (vm_socket < 0)
     std::perror("unable to accept connection on socket");
 
@@ -139,8 +123,7 @@ void VmsInterface::register_vm(std::string host_name, std::string vm_name, std::
   LOG("connection for VM " << vm_name << " established");
 }
 
-void VmsInterface::end_simulation(bool must_unlink, bool must_exit)
-{
+void VmsInterface::end_simulation(bool must_unlink, bool must_exit) {
   close(connection_socket);
   for (auto it : vm_sockets) {
     close(it.second);
@@ -155,13 +138,11 @@ void VmsInterface::end_simulation(bool must_unlink, bool must_exit)
   }
 }
 
-bool VmsInterface::vmActive()
-{
+bool VmsInterface::vmActive() {
   return (!vm_sockets.empty() && !simulate_until_any_stop) || (!a_vm_stopped && simulate_until_any_stop);
 }
 
-std::vector<Message*> VmsInterface::goTo(double deadline)
-{
+std::vector<Message *> VmsInterface::goTo(double deadline) {
   // Beforehand, forget about the VMs that bailed out recently.
   // We hope that the coordinator cleaned the SimGrid side in between
   vm_sockets_trash.clear();
@@ -175,9 +156,9 @@ std::vector<Message*> VmsInterface::goTo(double deadline)
   struct vsg_time vm_deadline = simgridToVmTime(deadline);
 
   // goto deadline message
-  auto time          = tansiv::Time(vm_deadline.seconds, vm_deadline.nseconds);
+  auto time = tansiv::Time(vm_deadline.seconds, vm_deadline.nseconds);
   auto goto_deadline = tansiv::CreateGotoDeadline(builder, &time);
-  auto msg           = tansiv::CreateFromTansivMsg(builder, tansiv::FromTansiv_GotoDeadline, goto_deadline.Union());
+  auto msg = tansiv::CreateFromTansivMsg(builder, tansiv::FromTansiv_GotoDeadline, goto_deadline.Union());
   builder.FinishSizePrefixed(msg);
 
   for (auto it : vm_sockets) {
@@ -186,69 +167,69 @@ std::vector<Message*> VmsInterface::goTo(double deadline)
   }
 
   // then, we pick up all the messages send by the VM until they reach the deadline
-  std::vector<Message*> messages;
+  std::vector<Message *> messages;
   LOG("getting the message send by the VMs");
 
   uint8_t scratch_buffer[SCRATCH_BUFFER_LEN];
 
   for (auto kv : vm_sockets) {
     std::string vm_name = kv.first;
-    int vm_socket       = kv.second;
+    int vm_socket = kv.second;
 
     // we loop until we get an at_deadline
     bool finished = false;
     while (!finished) {
       if (fb_recv(vm_socket, scratch_buffer, SCRATCH_BUFFER_LEN) < 0) {
-        LOG("can not receive the flags of VM "
-                        << vm_name << ". Forget about the socket that seem closed at the system level.");
+        LOG("can not receive the flags of VM " << vm_name
+                                               << ". Forget about the socket that seem closed at the system level.");
         close_vm_socket(vm_name);
         break;
       }
       auto msg = flatbuffers::GetRoot<tansiv::ToTansivMsg>(scratch_buffer);
       switch (msg->content_type()) {
 
-        case tansiv::ToTansiv_AtDeadline:
-          finished = true;
-          break;
+      case tansiv::ToTansiv_AtDeadline:
+        finished = true;
+        break;
 
-        case tansiv::ToTansiv_SendPacket: {
-          auto send_packet = msg->content_as_SendPacket();
-          // The returned pointer can be nullptr. This happens for instance when
-          // the content_type is inconsistent with the actual type of the
-          // content yes fbb doesn't prevent this inconsistency (fbb 2.0.0)
-          if (send_packet == nullptr) {
-            LOG("Deserialization error: type of content must be SendPacket");
-            break;
-          }
-          // Our schema use an fbb table (see packets.fbs) Fields on a table can
-          // be null however our protocol doesn't allow null fields so we're
-          // checking every single field before accepting the message
-          auto metadata = send_packet->metadata();
-          if (metadata == nullptr) {
-            LOG("Deserialization error: metadata can't be empty");
-            break;
-          }
-          auto time = send_packet->time();
-          if (time == nullptr) {
-            LOG("Deserialization error: time can't be empty");
-            break;
-          }
-          const flatbuffers::Vector<uint8_t>* payload = send_packet->payload();
-          if (payload == nullptr) {
-            LOG("Deserialization error: payload can't be empty");
-            break;
-          }
-          // build our own internal message structure and add it to the list of flying messages
-          auto message = new Message(time->seconds(), time->nseconds(), metadata->src(), metadata->dst(),
-                                     flatbuffers::VectorLength<uint8_t>(payload), (uint8_t*)payload->data());
-          messages.push_back(message);
+      case tansiv::ToTansiv_SendPacket: {
+        auto send_packet = msg->content_as_SendPacket();
+        // The returned pointer can be nullptr. This happens for instance when
+        // the content_type is inconsistent with the actual type of the
+        // content yes fbb doesn't prevent this inconsistency (fbb 2.0.0)
+        if (send_packet == nullptr) {
+          LOG("Deserialization error: type of content must be SendPacket");
           break;
         }
-        default:
-          LOG("Unknown message received from VM " << vm_name);
-          end_simulation();
-          finished = true;
+        // Our schema use an fbb table (see packets.fbs) Fields on a table can
+        // be null however our protocol doesn't allow null fields so we're
+        // checking every single field before accepting the message
+        auto metadata = send_packet->metadata();
+        if (metadata == nullptr) {
+          LOG("Deserialization error: metadata can't be empty");
           break;
+        }
+        auto time = send_packet->time();
+        if (time == nullptr) {
+          LOG("Deserialization error: time can't be empty");
+          break;
+        }
+        const flatbuffers::Vector<uint8_t> *payload = send_packet->payload();
+        if (payload == nullptr) {
+          LOG("Deserialization error: payload can't be empty");
+          break;
+        }
+        // build our own internal message structure and add it to the list of flying messages
+        auto message = new Message(time->seconds(), time->nseconds(), metadata->src(), metadata->dst(),
+                                   flatbuffers::VectorLength<uint8_t>(payload), (uint8_t *)payload->data());
+        messages.push_back(message);
+        break;
+      }
+      default:
+        LOG("Unknown message received from VM " << vm_name);
+        end_simulation();
+        finished = true;
+        break;
       }
     }
   }
@@ -263,24 +244,21 @@ std::vector<Message*> VmsInterface::goTo(double deadline)
   return messages;
 }
 
-std::string VmsInterface::getHostOfVm(std::string vm_name)
-{
+std::string VmsInterface::getHostOfVm(std::string vm_name) {
   if (vm_deployments.find(vm_name) == vm_deployments.end()) {
     return "";
   }
   return vm_deployments[vm_name];
 }
 
-void VmsInterface::close_vm_socket(std::string vm_name)
-{
+void VmsInterface::close_vm_socket(std::string vm_name) {
   int vm_socket = vm_sockets.at(vm_name);
   shutdown(vm_socket, SHUT_RDWR);
   close(vm_socket);
   vm_sockets_trash.push_back(vm_name);
   a_vm_stopped = true;
 }
-const std::vector<std::string> VmsInterface::get_dead_vm_hosts()
-{
+const std::vector<std::string> VmsInterface::get_dead_vm_hosts() {
   std::vector<std::string> dead_hosts;
   for (std::string vm : vm_sockets_trash) {
     dead_hosts.push_back(getHostOfVm(vm));
@@ -289,13 +267,12 @@ const std::vector<std::string> VmsInterface::get_dead_vm_hosts()
   return dead_hosts;
 }
 
-void VmsInterface::deliverMessage(Message* m)
-{
+void VmsInterface::deliverMessage(Message *m) {
   if (vm_sockets.find(m->dst) != vm_sockets.end()) {
     int socket = vm_sockets[m->dst];
 
     flatbuffers::FlatBufferBuilder builder(2048);
-    auto packet_meta    = tansiv::PacketMeta(m->src_enc, m->dst_enc);
+    auto packet_meta = tansiv::PacketMeta(m->src_enc, m->dst_enc);
     auto payload_offset = builder.CreateVector<uint8_t>(m->data, m->size);
     auto deliver_packet = tansiv::CreateDeliverPacket(builder, &packet_meta, payload_offset);
     auto msg =
@@ -306,15 +283,14 @@ void VmsInterface::deliverMessage(Message* m)
     LOG("message from vm " << m->src << " delivered to vm " << m->dst << " size=" << m->size);
   } else {
     LOG("message from vm " << m->src << " was not delivered to vm " << m->dst
-                                   << " because it already stopped its execution");
+                           << " because it already stopped its execution");
   }
   delete m;
 }
 
 Message::Message(uint64_t seconds, uint64_t nseconds, in_addr_t src_enc, in_addr_t dst_enc, uint32_t size,
-                 uint8_t* payload)
-    : seconds(seconds), nseconds(nseconds), src_enc(src_enc), dst_enc(dst_enc), size(size)
-{
+                 uint8_t *payload)
+    : seconds(seconds), nseconds(nseconds), src_enc(src_enc), dst_enc(dst_enc), size(size) {
   // -- compute sent time the sent_time
   this->sent_time = vmToSimgridTime(seconds, nseconds);
 
@@ -336,16 +312,14 @@ Message::Message(uint64_t seconds, uint64_t nseconds, in_addr_t src_enc, in_addr
 #endif
 };
 
-Message::Message(const Message& other)
-    : Message(other.seconds, other.nseconds, other.src_enc, other.dst_enc, other.size, other.data)
-{
+Message::Message(const Message &other)
+    : Message(other.seconds, other.nseconds, other.src_enc, other.dst_enc, other.size, other.data) {
 #ifdef LOG_MESSAGES
   printf("Copied Message[%p]: size=%d, data@%p from message[%p]\n", this, this->size, this->data, &other);
 #endif
 }
 
-Message::Message(Message&& other) : data(nullptr)
-{
+Message::Message(Message &&other) : data(nullptr) {
   // uses the assignement
   *this = std::move(other);
 #ifdef LOG_MESSAGES
@@ -353,30 +327,29 @@ Message::Message(Message&& other) : data(nullptr)
 #endif
 }
 
-Message& Message::operator=(Message&& other)
-{
+Message &Message::operator=(Message &&other) {
   if (this != &other) {
     delete[] this->data;
-    this->seconds   = other.seconds;
-    this->nseconds  = other.nseconds;
-    this->src_enc   = other.src_enc;
-    this->dst_enc   = other.dst_enc;
-    this->size      = other.size;
+    this->seconds = other.seconds;
+    this->nseconds = other.nseconds;
+    this->src_enc = other.src_enc;
+    this->dst_enc = other.dst_enc;
+    this->size = other.size;
     this->sent_time = other.sent_time;
-    this->src       = other.src;
-    this->dst       = other.dst;
-    this->data      = other.data;
+    this->src = other.src;
+    this->dst = other.dst;
+    this->data = other.data;
 
     other.data = nullptr;
   }
 #ifdef LOG_MESSAGES
-  printf("Moved assigned Message[%p]: size=%d, data@%p from message[%p]\n", this, this->size, this->data, &other);
+  fprintf(stderr, "Moved assigned Message[%p]: size=%d, data@%p from message[%p]\n", this, this->size, this->data,
+          &other);
 #endif
   return *this;
 }
 
-Message::~Message()
-{
+Message::~Message() {
   if (this->data != nullptr) {
     delete[] this->data;
 #ifdef LOG_MESSAGES
--- src/coordinator/VmsInterface.hpp
+++ src/coordinator/VmsInterface.hpp
@@ -18,10 +18,10 @@ struct vsg_time {
 
 class Message {
 public:
-  Message(uint64_t seconds, uint64_t nseconds, in_addr_t src_enc, in_addr_t dst_enc, uint32_t size, uint8_t* payload);
-  Message(const Message& other);
-  Message(Message&& other);
-  Message& operator=(Message&& other);
+  Message(uint64_t seconds, uint64_t nseconds, in_addr_t src_enc, in_addr_t dst_enc, uint32_t size, uint8_t *payload);
+  Message(const Message &other);
+  Message(Message &&other);
+  Message &operator=(Message &&other);
   ~Message();
   uint64_t seconds;
   u_int64_t nseconds;
@@ -34,7 +34,7 @@ public:
   std::string src;
   std::string dst;
   // this will be dynamically allocated according to size
-  uint8_t* data;
+  uint8_t *data;
 };
 
 class VmsInterface {
@@ -43,9 +43,9 @@ public:
   VmsInterface(std::string connection_socket_name, bool stop_condition = false);
   ~VmsInterface();
   bool vmActive();
-  std::vector<Message*> goTo(double deadline);
+  std::vector<Message *> goTo(double deadline);
   std::string getHostOfVm(std::string vm_name);
-  void deliverMessage(Message* m);
+  void deliverMessage(Message *m);
   void end_simulation(bool must_unlink = true, bool must_exit = true);
   void register_vm(std::string host_name, std::string vm_name, std::string file, std::vector<std::string> args);
   const std::vector<std::string> get_dead_vm_hosts();
